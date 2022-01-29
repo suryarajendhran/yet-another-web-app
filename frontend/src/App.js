3e9c44ff989afc432cba2db405f974a945bd6028
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2022-01-29 19:25:02 +0530

    feat: transfer data between drops

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import data from './data.json';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data.json";
 
 function App() {
   console.log(data);
@@ -8,12 +8,47 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="gallery">
-          {data.map(({title, position, type}) => {
+          {data.map(({ title, position, type }) => {
             return (
-              <div className="card">
+              <div
+                key={title}
+                className="card"
+                draggable="true"
+                onDragStart={(event) => {
+                  event.target.style.opacity = 0.4;
+
+                  event.dataTransfer.effectAllowed = 'move';
+                  event.dataTransfer.setData("text/plain", title);
+                }}
+                onDragEnd={(event) => {
+                  event.target.style.opacity = 1;
+
+                  //TODO: Remove .over from all items
+                }}
+                onDragEnter={(event) => {
+                  event.target.classList.add("over");
+                }}
+                onDragLeave={(event) => {
+                  event.target.classList.remove("over");
+                }}
+                onDragOver={(event) => {
+                  if (event.preventDefault) {
+                    event.preventDefault();
+                  }
+                  return false;
+                }}
+                onDrop={(event) => {
+                  event.stopPropagation();
+
+                  const droppedItemTitle = event.dataTransfer.getData("text/plain");
+
+                  console.log(`${droppedItemTitle} dropped on ${title}`);
+
+                }}
+              >
                 {title}
               </div>
-            )
+            );
           })}
         </div>
       </header>
