2bc7b33d2905ac6a9a337cd730797fbe76866253
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2022-01-29 17:13:37 +0530

    feat: build basic gallery

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="gallery">
+          {data.map(({title, position, type}) => {
+            return (
+              <div className="card">
+                {title}
+              </div>
+            )
+          })}
+        </div>
       </header>
     </div>
   );
